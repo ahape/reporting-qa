dde0ad0d13a60e12521b74a5f72918b319904076
Alan Hape <ahape@brightmetrics.com>
2023-05-03 00:23:10 -0700

    update

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #!/usr/local/bin/python3.11
-import sys, os, argparse, datetime, logging, yaml, random
+import sys, os, argparse, datetime, logging, yaml, random, markdown
 from datetime import datetime as dt
 
-OUTPUT_FILE_NAME = "checklist.html"
+OUTPUT_FILE_NAME = "checklist"
 logging.basicConfig(format="%(asctime)s [%(levelname)s]: %(message)s")
 logger = logging.getLogger("main")
 environment = None
@@ -17,132 +17,119 @@ def create_link(link):
   splice = link.rindex("#") if "#" in link else len(link)
   return link[:splice] + bust + link[splice:]
 
-def load_stylesheet():
-  with open("style.css", "r", encoding="utf8") as stream:
-    return stream.read()
-
-def load_yaml():
-  groups = []
-
-  with open("tests.yaml", "r", encoding="utf8") as stream:
+def load_yaml(path):
+  with open(path, "r", encoding="utf8") as stream:
     try:
-      groups = yaml.safe_load(stream)
+      return yaml.safe_load(stream)
     except yaml.YAMLError as ex:
       logger.error("%s", ex)
+  return None
 
-  return [TestGroup(group["category"], group["tests"]) for group in groups]
+def load_tests():
+  groups = []
+
+  for group_name in os.listdir("tests"):
+    group = TestGroup(group_name)
+    groups.append(group)
+    fullpath = "tests/" + group_name
+    for test_name in os.listdir(fullpath):
+      json = load_yaml(fullpath + "/" + test_name)
+      group.tests.append(Test(test_name, **json))
+
+  return groups
+
+def md_hr(title, bar):
+  return "".zfill(len(title)).replace("0", bar)
 
 class TestGroup:
-  def __init__(self, name, tests):
-    self.name = name
-    self.tests = [Test(**t) for t in tests]
+  def __init__(self, name):
+    self.name = name.replace("_", " ") + " tests"
+    self.tests = []
 
 class Test:
-  def __init__(self, **kwargs):
-    self.id = random.randint(0,999_999)
+  def __init__(self, name, **kwargs):
+    self.name = name.replace(".yaml", "").replace("_", " ")
     self.link = create_link(kwargs["link"])
-    self.desc = kwargs["desc"]
-    self.traits = kwargs["traits"]
+    self.steps = ["  * " + bullet for bullet in kwargs["desc"]]
 
-test_row_template = r"""
-<tr>
-  <td><input type="checkbox" disabled="disabled" id="{id}" /></td>
-  <td class="fixed">
-    <a href="{link}"
-       target="_blank"
-       onclick="x=document.getElementById('{id}');x.checked=true;x.disabled=false">
-       link
-    </a>
-  </td>
-  <td>
-    <pre>{desc}</pre>
-  </td>
-</tr>
+test_template = r"""
+{name}
+{hr}
+  * [(link to test)]({link})
+{steps}
 """
 
-test_table_template = r"""
-<table>
-  <caption>
-    <h2>{title}</h2>
-  </caption>
-  <tbody>
-    {rows}
-  </tbody>
-</table>
-"""
-
-checklist_template = r"""
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <title>QA Checklist</title>
-    <style>
-      {stylesheet}
-    </style>
-  </head>
-  <body>
-    <h1>QA Checklist</h1>
-    {tests}
-  </body>
-</html>
+test_group_template = r"""
+{name}
+{hr}
+{tests}
 """
 
 def timestamp():
   return dt.strftime(dt.now(), "%Y%m%d%H%M%S")
 
-def render(args):
-  def render_rows(tests):
+def render():
+  def render_tests(tests):
     rendered = []
     for t in tests:
       rendered.append(
-        test_row_template.format(
-          id=t.id,
+        test_template.format(
+          name=t.name,
           link=t.link,
-          desc=t.desc,
-          traits=t.traits))
+          hr=md_hr(t.name, "-"),
+          steps="\n".join(t.steps)))
     return "".join(rendered)
 
-  def render_table(test_group):
-    return test_table_template.format(
-      title=test_group.name,
-      rows=render_rows(test_group.tests))
+  def render_test_group(test_group):
+    return test_group_template.format(
+      name=test_group.name,
+      hr=md_hr(test_group.name, "="),
+      tests=render_tests(test_group.tests))
 
-  def render_tables(test_groups):
-    rendered = []
-    for tg in test_groups:
-      rendered.append(render_table(tg))
+  def _render(test_groups):
+    return "".join([render_test_group(tg) for tg in test_groups])
 
-    return checklist_template.format(tests="".join(rendered), stylesheet=load_stylesheet())
-
-  return render_tables(load_yaml())
+  return _render(load_tests())
 
 def get_args():
   args = argparse.ArgumentParser()
   args.add_argument("-k", "--keep", action="store_true")
   args.add_argument("-v", "--verbose", action="store_true")
   args.add_argument("-e", "--environment")
+  args.add_argument("--html", action="store_true")
   return args.parse_args()
 
 def archive_existing_checklist():
-  if os.path.exists(OUTPUT_FILE_NAME):
+  filename = OUTPUT_FILE_NAME + ".md"
+  if os.path.exists(filename):
     contents = ""
-    with open(OUTPUT_FILE_NAME, "r", encoding="utf8") as existing_file:
+    with open(filename, "r", encoding="utf8") as existing_file:
       contents = existing_file.read()
 
     if not os.path.exists("archives"):
       os.mkdir("archives")
 
-    with open(f"archives/n{timestamp()}_{OUTPUT_FILE_NAME}", "w", encoding="utf8") as archived_file:
+    with open(f"archives/n{timestamp()}_{filename}", "w", encoding="utf8") as archived_file:
       archived_file.write(contents)
 
 def create_checklist_file(args):
+  filename = OUTPUT_FILE_NAME + ".md"
+
   if args.keep:
     archive_existing_checklist()
 
-  with open(OUTPUT_FILE_NAME, "w", encoding="utf8") as file:
-    file.write(render(args))
-    logger.debug("DONE")
+  md = ""
+
+  with open(filename, "w", encoding="utf8") as file:
+    md = render()
+    file.write(md)
+
+  if args.html:
+    with open(OUTPUT_FILE_NAME + ".html", "w", encoding="utf8") as file:
+      html = markdown.markdown(md)
+      file.write(html)
+
+  logger.debug("DONE")
 
 def main(args):
   if args.verbose:
